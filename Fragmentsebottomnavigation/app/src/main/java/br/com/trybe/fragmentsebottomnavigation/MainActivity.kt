843855f305f180f70ff4f7dee933bd3cad4495b1
Lucas Israel <lisraelalves92@gmail.com>
2024-07-01 16:38:40 -0300

    bottom navigation

--- Fragmentsebottomnavigation/app/src/main/java/br/com/trybe/fragmentsebottomnavigation/MainActivity.kt
+++ Fragmentsebottomnavigation/app/src/main/java/br/com/trybe/fragmentsebottomnavigation/MainActivity.kt
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private val fragmentTwo = FragmentTwo()
     private val mFragOneBtn: Button by lazy { findViewById(R.id.main_bt_frag1) }
     private val mFragTwoBtn: Button by lazy { findViewById(R.id.main_bt_frag2) }
+    private val mNavBar: BottomNavigationView by lazy { findViewById(R.id.main_bn_navmenu) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         mFragOneBtn.setOnClickListener(this)
         mFragTwoBtn.setOnClickListener(this)
+
+        mNavBar.setOnItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.item_1 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_view, fragmentOne).commit()
+                }
+                R.id.item_2 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_view, fragmentTwo).commit()
+                }
+            }
+        }
     }
 
     override fun onClick(view: View?) {
